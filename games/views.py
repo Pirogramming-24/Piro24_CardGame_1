fcfbb1ead39354db9c4f886244dedcf893114717
SeungKu-Kang <yoyuman@gmail.com>
2026-01-19 02:51:44 +0900

    feat: pk수정->db조회 건너뛰기

--- games/views.py
+++ games/views.py
@@ -14,7 +14,8 @@ def select_five_cards():
     selected_numbers.sort()
     return selected_numbers
 
-def generateGame(request,pk):
+def generateGame(request):
+    pk = request.user.pk
     if 'five_cards' not in request.session:
         print('new')
         request.session['five_cards'] = select_five_cards()
@@ -40,7 +41,7 @@ def generateGame(request,pk):
             print("카드를 선택해주세요")
             return redirect(request.path)
 
-        Attacker = User.objects.get(id=pk)
+        Attacker = request.user
         Defender = User.objects.get(id=defender_id)
         isBiggerScoreWin = rd.choice([True,False])
         isGameOngoing = True
@@ -53,12 +54,13 @@ def generateGame(request,pk):
         )
         del request.session['five_cards']
         print('delete')
-        return redirect('games:gameList',pk=pk)
+        return redirect('games:gameList')
     
     return render(request,'games/startPage.html',context)
 
 
-def gameList(request,pk):
+def gameList(request):
+    pk = request.user.pk
     Games = Game.objects.all()
     context = {
         'Games':Games,
@@ -70,7 +72,7 @@ def gameList(request,pk):
         print(game_id)
         game_DB = Game.objects.get(id=game_id)
         game_DB.delete()
-        return redirect('games:gameList',pk=pk)
+        return redirect('games:gameList')
     return render(request,'games/gameList.html',context)
 
 
@@ -137,7 +139,9 @@ def counter_attack(request, pk) :
             game.save()
 
         return redirect('games:detail', pk=pk)
-    
+    else :
+        context = {'game': game}
+        return render(request, 'games/gameCounter.html', context)
     # POST 요청이 아니면 상세 페이지로 리다이렉트
     return redirect('games:detail', pk=pk)
 
