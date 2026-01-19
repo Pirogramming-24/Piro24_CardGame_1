35a51ec5371317ef23efe8eda863a7ee8a6505af
chanwoongYoon <cwyoon9718@gamil.com>
2026-01-19 14:17:39 +0900

    배포 완

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-fcxx-okxyxu*hb=*9&fh@#j!n9+6s7-s7u#wqbtge%-v0%w!1=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', '127.0.0.1', 'localhost']
 
 
 # Application definition
