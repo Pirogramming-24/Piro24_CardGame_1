e8a71846d63533a820d993320dca22fd10bf39b8
SeungKu-Kang <170948850+SeungKu-Kang@users.noreply.github.com>
2026-01-19 17:05:59 +0900

    feat: 상단바 logout 버튼 구현

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.shortcuts import render, redirect 
+from django.contrib.auth import logout
 
 # Create your views here.
 def login_view(request):
@@ -6,3 +8,7 @@ def login_view(request):
 
 def signup_view(request):
     return render(request, "accounts/signup.html")
+
+def logout_view(request):
+    logout(request)  
+    return redirect('accounts:login')
